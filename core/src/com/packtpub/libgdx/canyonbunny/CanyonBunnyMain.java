447a38248642f504db5a0cd9a78df29d2d9bebe6
Khang <nguyenbin332@gmail.com>
2021-09-17 07:55:07 +0700

    thiet lap game canonbunny

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,31 +1,60 @@
 package com.packtpub.libgdx.canyonbunny;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private static final String TAB = CanyonBunnyMain.class.getName();
+	private WorldController WorldController;
+	private WorldRenderer WorldRenderer;
+	
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+		WorldController = new WorldController();
+		WorldRenderer = new WorldRenderer(WorldRenderer);
+		
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+	public void render() {
+    	WorldController.update(Gdx.graphics.getDeltaTime());
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255, 0xff/255.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		WorldRenderer.render();
+    }
+    
+    @Override 
+    public void resize(int width, int height) {
+    	// TODO Auto-generated method stub
+    	WorldRenderer.resize(width, height);
+    	
+    }
+    
+    @Override 
+    public void pause() {
+    	// TODO Auto-generated method stub
+    	super.pause();
+    	
+    }
+    
+    @Override 
+    public void resume() {
+    	// TODO Auto-generated method stub
+    	super.resume();
+    	
+    }
+    
+    @Override 
+    public void dispose() {
+    	WorldRenderer.dispose();
+    	
+    }
+	
+	//@Override
+	
 }
